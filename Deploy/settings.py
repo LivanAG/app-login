4f19338958da855cd0987a1dc5f35a672bfd10cf
Livan <livanar00@gmail.com>
2021-02-17 12:33:10 -0500

    Listo para deploy2

--- Deploy/settings.py
+++ Deploy/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qhw1ov4p%v426(3n-&y2v8(vc&fplu21gs%j=s@e0wr2_0ptyt'
-
+#SECRET_KEY = 'qhw1ov4p%v426(3n-&y2v8(vc&fplu21gs%j=s@e0wr2_0ptyt'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = []
 
 
