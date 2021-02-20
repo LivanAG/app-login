5b00b372019eb5cfa81778bbdaef4d5c205be9a3
Livan <livanar00@gmail.com>
2021-02-19 19:53:37 -0500

    probando 4

--- apps/Controlador/views.py
+++ apps/Controlador/views.py
@@ -7,10 +7,10 @@ class IndexView(TemplateView):
     template_name='Controlador/index.html'
     def get_context_data(self,*args,**kwargs):
         l = int(os.environ.get('TIMES'))
-        
+        k = str(os.environ.get('DATABASE_URL'))
         context = super().get_context_data(*args,**kwargs)
         context['saludo'] = "Hola"
         context['l'] = l
-       
+        context['k'] = k
         
         return context
