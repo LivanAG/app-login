9ae50b8a1a8e56f37614898a00eccfb2c6d4a2cf
Livan <livanar00@gmail.com>
2021-02-19 19:43:04 -0500

    Probando os

--- apps/Controlador/views.py
+++ apps/Controlador/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import os
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name='Controlador/index.html'
     def get_context_data(self,*args,**kwargs):
+        l = int(os.environ.get('TIMES',3))
         context = super().get_context_data(*args,**kwargs)
         context['saludo'] = "Hola"
+        context['l'] = l
         return context
